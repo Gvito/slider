bea14ccccc515c67394b49a169a02d211285ba32
gino <ginovito@live.fr>
2019-03-06 12:22:04 +0100

    add function display none

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+// variable all img
+var allImg = document.getElementsByClassName("allImg");
+// hide all img
+allImg[0].style.display = "none";
+// view first img
+var imgOne = document.getElementById("imgOne");
+imgOne[0].style.display = "block";
