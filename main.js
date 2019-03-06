1f99ea41d50e22d155be474a78e33e6639ee0e35
gino <ginovito@live.fr>
2019-03-06 16:33:04 +0100

    end carousel img and add text on each picture

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-// variable all img
-var allImg = document.getElementsByClassName("allImg");
-// hide all img
-allImg[0].style.display = "none";
-// view first img
-var imgOne = document.getElementById("imgOne");
-imgOne[0].style.display = "block";
+// stock the var picture 0 in the index
+var pictureIndex = 0;
+// actived function
+showCarousel();
+// Carousel
+function showCarousel() {
+  // variable of all img
+  var picture = document.getElementsByClassName("pictureDiv");
+  // hide all img
+  for (var i = 0; i < picture.length; i++) {
+    picture[i].style.display = "none";
+  }
+  pictureIndex++;
+  // if the pictureIndex > the picture 1 so +1 on pictureIndex
+  if (pictureIndex > picture.length) {
+    pictureIndex = 1;
+  }
+  // view picture of the boucle and delete the precedent pictureIndex view
+  picture[pictureIndex-1].style.display = "block";
+  // reload the showCarousel function all the 3s
+  setTimeout(showCarousel, 3000);
+}
